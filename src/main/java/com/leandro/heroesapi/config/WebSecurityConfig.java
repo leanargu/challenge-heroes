49852c5379c75a4ed7f1cc3cd19b7d77b80d71f0
Leandro Arguello <leanarguello012@gmail.com>
2022-11-18 23:16:37 -0300

    Security test getAllHeroes without auth and return unauthorized upload configuration

--- src/main/java/com/leandro/heroesapi/config/WebSecurityConfig.java
+++ src/main/java/com/leandro/heroesapi/config/WebSecurityConfig.java
@@ -1,2 +1,46 @@
-package com.leandro.heroesapi.config;public class WebSecurityConfig {
+package com.leandro.heroesapi.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig {
+
+    @Value("${admin-user.username}")
+    private String adminUsername;
+    @Value("${admin-user.password}")
+    private String adminPassword;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username(adminUsername)
+                .password(adminPassword)
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csfr -> csfr.disable())
+                .authorizeRequests(auth -> {
+                    auth.antMatchers("/api/v1/heroe").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
 }
