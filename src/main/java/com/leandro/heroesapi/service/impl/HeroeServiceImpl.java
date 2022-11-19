040a96dfc334a957d2c35d8d395b53e21fbc1c19
Leandro Arguello <leanarguello012@gmail.com>
2022-11-18 22:50:00 -0300

    Cache test getAllHeroes pass

--- src/main/java/com/leandro/heroesapi/service/impl/HeroeServiceImpl.java
+++ src/main/java/com/leandro/heroesapi/service/impl/HeroeServiceImpl.java
@@ -4,6 +4,8 @@ import com.leandro.heroesapi.exception.HeroesNotFoundException;
 import com.leandro.heroesapi.model.Heroe;
 import com.leandro.heroesapi.repository.HeroeRepository;
 import com.leandro.heroesapi.service.HeroeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,12 +13,20 @@ import java.util.List;
 @Service
 public class HeroeServiceImpl implements HeroeService {
 
+    @Autowired
     private HeroeRepository heroeRepository;
 
+    @Autowired
     public HeroeServiceImpl(HeroeRepository heroeRepository){
         this.heroeRepository = heroeRepository;
     }
 
+    @Autowired
+    public void setHeroeRepository(HeroeRepository heroeRepository) {
+        this.heroeRepository = heroeRepository;
+    }
+
+    @Cacheable("all_heroes")
     public List<Heroe> getAllHeroes() {
         List<Heroe> foundHeroes = heroeRepository.findAll();
 
