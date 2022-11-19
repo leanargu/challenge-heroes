cbeb8b064b601cc0b5324d5511412f04b852c47f
Leandro Arguello <leanarguello012@gmail.com>
2022-11-18 21:58:25 -0300

    Controller test getAllHeroes pass

--- src/main/java/com/leandro/heroesapi/controller/HeroeController.java
+++ src/main/java/com/leandro/heroesapi/controller/HeroeController.java
@@ -1,2 +1,25 @@
-package com.leandro.heroesapi.controller;public class HeroeController {
+package com.leandro.heroesapi.controller;
+
+import com.leandro.heroesapi.model.Heroe;
+import com.leandro.heroesapi.service.HeroeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/heroe")
+public class HeroeController {
+
+    @Autowired
+    private HeroeService heroeService;
+
+    @GetMapping("/all")
+    public List<Heroe> getAllHeroes() {
+        return heroeService.getAllHeroes();
+    }
+
 }
--- src/main/java/com/leandro/heroesapi/service/impl/HeroeServiceImpl.java
+++ src/main/java/com/leandro/heroesapi/service/impl/HeroeServiceImpl.java
@@ -4,9 +4,11 @@ import com.leandro.heroesapi.exception.HeroesNotFoundException;
 import com.leandro.heroesapi.model.Heroe;
 import com.leandro.heroesapi.repository.HeroeRepository;
 import com.leandro.heroesapi.service.HeroeService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class HeroeServiceImpl implements HeroeService {
 
     private HeroeRepository heroeRepository;
