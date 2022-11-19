e5d77c26db5aaccf8171549fcfdf3dfa0e3c7cc8
Leandro Arguello <leanarguello012@gmail.com>
2022-11-18 21:53:55 -0300

    create controller test getAllHeroes fail

---
+++ src/test/java/com/leandro/heroesapi/controller/HeroeControllerTest.java
@@ -0,0 +1,37 @@
+package com.leandro.heroesapi.controller;
+
+import com.leandro.heroesapi.repository.HeroeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class HeroeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HeroeRepository heroeRepository;
+
+    @Test
+    void getAllHeroes_withHeroes_returnOkWithHeroes() throws Exception {
+        mockMvc.perform(get("/api/v1/heroe/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "[{\"id\":1,\"name\":\"Hulk\"}," +
+                                "{\"id\":2,\"name\":\"Superman\"}," +
+                                "{\"id\":3,\"name\":\"Batman\"}]"
+                ));
+    }
+
+}
